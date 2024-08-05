efa464f31fd25aa2f033b7f42c210c0902751ad7
qwer <76216352+3309446352@users.noreply.github.com>
2024-08-06 02:46:52 +0800

    第四提交

---
+++ components/My/We.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="window">
+    <div data-v-14565bac="" class="min-w-[180px] max-w-[70%] ml-[120px]">
+      <h3 class="my-8" _msttexthash="9518769" _msthash="2998">个人信息</h3>
+      <form class="el-form el-form--default el-form--label-top">
+        <div class="el-form-item asterisk-left el-form-item--label-top" role="group" aria-labelledby="el-id-226-453">
+          <div id="el-id-226-453" class="el-form-item__label">
+            <font _mstmutation="1" _msttexthash="4229524" _msthash="2999">头像</font>
+          </div>
+          <div class="el-form-item__content"><span class="el-avatar el-avatar--circle"
+                                                   style="--el-avatar-size: 80px;"><img src="https://avatars.githubusercontent.com/u/44761321"
+                                                                                        style="object-fit: cover;"></span>
+            <div><!--v-if-->
+              <div class="el-upload el-upload--text" tabindex="0"><!--v-if--><button aria-disabled="false"
+                                                                                     type="button" class="el-button is-plain ml-4"><!--v-if--><span class=""><svg
+                  xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                  aria-hidden="true" role="img" width="1em" height="1em" viewBox="0 0 24 24"
+                  style="outline: none;">
+                                    <path fill="currentColor"
+                                          d="M3 19h18v2H3v-2ZM13 5.828V17h-2V5.828L4.929 11.9l-1.414-1.414L12 2l8.485 8.485l-1.414 1.415L13 5.827Z">
+                                    </path>
+                                </svg><span class="ml-2" _msttexthash="10466846"
+                                            _msthash="3000">更新头像</span></span></button><input class="el-upload__input"
+                                                                                                  name="file" accept="image/*" type="file"></div><!--v-if--><!--v-if-->
+            </div>
+          </div>
+        </div>
+        <div class="el-form-item is-required asterisk-left el-form-item--label-top"><label id="el-id-226-454"
+                                                                                           for="el-id-226-461" class="el-form-item__label">
+          <font _mstmutation="1" _msttexthash="5627479" _msthash="2992">昵称</font>
+        </label>
+          <div class="el-form-item__content">
+            <div class="el-input"><!-- input --><!-- prepend slot --><!--v-if-->
+              <div class="el-input__wrapper" tabindex="-1"><!-- prefix slot --><!--v-if--><input
+                  class="el-input__inner" type="text" autocomplete="off" tabindex="0" placeholder="请输入昵称"
+                  id="el-id-226-461" _mstplaceholder="17386408"
+                  _msthash="3001"><!-- suffix slot --><!--v-if--></div><!-- append slot --><!--v-if-->
+            </div>
+          </div>
+        </div>
+        <div class="el-form-item asterisk-left el-form-item--label-top"><label id="el-id-226-455" for="el-id-226-462"
+                                                                               class="el-form-item__label">
+          <font _mstmutation="1" _msttexthash="6663618" _msthash="2991">邮箱</font>
+        </label>
+          <div class="el-form-item__content">
+            <div class="el-autocomplete w-full el-tooltip__trigger el-tooltip__trigger" role="combobox"
+                 aria-haspopup="listbox" aria-expanded="false" aria-owns="el-id-226-457"
+                 aria-controls="el-id-226-456">
+              <div class="el-input el-input--suffix"><!-- input --><!-- prepend slot --><!--v-if-->
+                <div class="el-input__wrapper" tabindex="-1"><!-- prefix slot --><!--v-if--><input
+                    class="el-input__inner" type="text" autocomplete="off" tabindex="0" placeholder="请输入邮箱"
+                    role="textbox" aria-autocomplete="list" aria-controls="id"
+                    aria-activedescendant="el-id-226-457-item--1" id="el-id-226-462"
+                    _mstplaceholder="18864105" _msthash="3002"><!-- suffix slot --><!--v-if--></div>
+                <!-- append slot --><!--v-if-->
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="el-form-item asterisk-left el-form-item--label-top"><label id="el-id-226-458" for="el-id-226-463"
+                                                                               class="el-form-item__label">
+          <font _mstmutation="1" _msttexthash="14482247" _msthash="2990">联系电话</font>
+        </label>
+          <div class="el-form-item__content">
+            <div class="el-input el-input--suffix"><!-- input --><!-- prepend slot --><!--v-if-->
+              <div class="el-input__wrapper" tabindex="-1"><!-- prefix slot --><!--v-if--><input
+                  class="el-input__inner" type="text" autocomplete="off" tabindex="0" placeholder="请输入联系电话"
+                  id="el-id-226-463" _mstplaceholder="29098940"
+                  _msthash="3003"><!-- suffix slot --><!--v-if--></div><!-- append slot --><!--v-if-->
+            </div>
+          </div>
+        </div>
+        <div class="el-form-item asterisk-left el-form-item--label-top"><label id="el-id-226-459" for="el-id-226-464"
+                                                                               class="el-form-item__label">
+          <font _mstmutation="1" _msttexthash="4974840" _msthash="2989">简介</font>
+        </label>
+          <div class="el-form-item__content">
+            <div class="el-textarea"><!-- input --><!-- textarea --><textarea class="el-textarea__inner"
+                                                                              maxlength="56" tabindex="0" autocomplete="off" placeholder="请输入简介" rows="2" id="el-id-226-464"
+                                                                              style="min-height: 136px; height: 136px;" _mstplaceholder="16515603"
+                                                                              _msthash="3004"></textarea><span class="el-input__count" _msttexthash="29770" _msthash="3005">12
+                        / 56</span></div>
+          </div>
+        </div><button aria-disabled="false" type="button" class="el-button el-button--primary"><!--v-if--><span
+          class="">
+                <font _mstmutation="1" _msttexthash="10707567" _msthash="3006">更新信息</font>
+            </span></button>
+      </form>
+      <div class="el-overlay" _msthidden="1" style="z-index: 2126; display: none;">
+        <div role="dialog" aria-modal="true" aria-label="编辑头像" aria-describedby="el-id-226-460"
+             class="el-overlay-dialog" _msthidden="A" _mstaria-label="12144028" _msthash="3007"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+defineComponent({
+  name: "We"
+})
+</script>
