25861fa77cb0599d50b8f17942acc83757bed767
qwer <76216352+3309446352@users.noreply.github.com>
2024-08-08 15:56:27 +0800

    editor修订，卡片修订

---
+++ pages/editor/index.vue
@@ -0,0 +1,33 @@
+
+
+<template>
+  <el-card shadow="never">
+    <el-collapse  accordion >
+      <MdEditor v-model="text" :theme="theme"/>
+
+    </el-collapse>
+  </el-card>
+</template>
+
+<style lang="scss" scoped>
+:deep(.el-collapse-item__header) {
+  padding-left: 10px;
+}
+</style>
+<script setup lang="ts">
+import { ref } from "vue";
+// import  EditorBase from "./components/EditorBase.vue";
+// import   EditorMulti from "./components/EditorMulti.vue";
+// import  EditorUpload from "./components/EditorUpload.vue";
+defineOptions({
+  name: "Editor"
+});
+definePageMeta(
+    { title: '编辑器' }
+)
+import { MdEditor } from 'md-editor-v3';
+import 'md-editor-v3/lib/style.css';
+
+const text = ref('# Hello Editor');
+const theme = ref('light');
+</script>
